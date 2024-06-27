12327919de1d082f4d816b8ca170b9a3468fd445
sebaxx85 <isurus.1313@gmail.com>
2024-06-26 22:19:41 -0400

    act home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,20 @@ import { SelectLevelComponent } from './components/select-level/select-level.com
 import { QuestionsComponent } from './components/questions/questions.component';
 import { ResultComponent } from './components/result/result.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
+//Angular Material
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
 
 @NgModule({
   declarations: [
@@ -18,7 +29,9 @@ import { MatCardModule } from '@angular/material/card';
     HomeComponent,
     SelectLevelComponent,
     QuestionsComponent,
-    ResultComponent
+    ResultComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -26,9 +39,18 @@ import { MatCardModule } from '@angular/material/card';
     BrowserAnimationsModule,
     //Angular Material
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatTableModule,
+    MatProgressBarModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatNativeDateModule
+
   ],
-  providers: [],
+  providers: [
+    //{provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+ 
+  
+  title = "Seleccion nivel ";
+  codPrograma = "Ingreso Plataforma";
+  // sucursal = localStorage.getItem('Sucursal') || '';
+  // DEFAULT_COD_SUCURSAL = JSON.parse(this.sucursal);
+  nombreSucursal = ''
+  
+  dataTable = []
+  form!: FormGroup
+  loading = false
+  today = new Date();
+
+  estadoProceso = [
+    { label: 'Recepcionado', value: 'REC' },
+    { label: 'Finalizado', value: 'FIN' },
+    { label: 'En Proceso', value: 'EPR' },
+    { label: 'Anulado', value: 'ANU' },
+  ] 
+
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+
+  
+  constructor(private fb: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.initForm();
+
+    // cualquier cambio el formulario se resetea la tabla
+    this.form.valueChanges.pipe(
+
+
+    ).subscribe({
+      next: data => {
+        this.dataTable = [];
+      }
+    })
+
+  }
+
+  initForm() {
+    this.form = this.fb.group({
+      codSucursal: [''],
+      idAmbulatorio: [''],
+      fechaInicio: [this.today.toISOString().split('T')[0]],
+      fechaTermino: [''],
+      codUnidadEnvio: [''],
+      especialidad: [''],
+      codTipoBiopsia: [''],
+      codDeriva: [''],
+    })
+  }
+
+  buscarPuntaje(){}
+
+  // buscarTrazabilidad() {
+
+  //   const form = this.form.getRawValue();
+
+  //   this.loading = true;
+  //   this.rxp047CService.getTrazabilidadMuestrasByPaciente({
+  //     idAmbulatorio: form.idAmbulatorio ? form.idAmbulatorio : 0,
+  //     idAtencion: form.idAtencion ? form.idAtencion : 0,
+  //     tipoPaciente: form.tipoPaciente,
+  //     fechaInicio: moment(form.fechaInicio).format('DD/MM/YYYY'),
+  //     fechaTermino: moment(form.fechaTermino).format('DD/MM/YYYY'),
+  //     estadoProceso: 'REC',
+  //     codFamilia: form.especialidad ? form.especialidad : 0,
+  //     codTipobiopsia: form.codTipoBiopsia ? form.codTipoBiopsia : 0,
+  //     nroCarton: form.nroCarton ? form.nroCarton : 0,
+  //     codUnidadEnvio: form.codUnidadEnvio ? form.codUnidadEnvio : 0,
+
+  //     codDeriva: form.codDeriva ? form.codDeriva : 0,
+  //   }).pipe(
+  //     finalize(() => this.loading = false)
+  //   ).subscribe((data: any) => {
+  //     if (data.codRespuesta === 0) {
+
+  //       this.dataTable = data.muestras as any[];
+  //       this.form.patchValue(this.dataTable);
+  //     }
+  //   })
+  // }
+
+
+  // seleccioneUnidad(cod: number) {
+  //   this.form.get('codUnidadEnvio').setValue(cod);
+  // }
+
+  // onSelecteCodSucursal(event: Sucursal) {
+  //   this.form.get('codSucursal').setValue(event.codSucursal);
+  // }
+
+
+  resetAll() {
+    this.form.reset({
+     // codSucursal: this.DEFAULT_COD_SUCURSAL,
+      tipoPaciente: 'T',
+    });
+    this.dataTable = [];
+  }
+
+  ngOnDestroy(): void {
+    // this.destroy$.next();
+    // this.destroy$.complete();
+  }
 
 }
