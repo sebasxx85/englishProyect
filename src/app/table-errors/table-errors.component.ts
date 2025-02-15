6e474895e65abc8d6004aaca48d48086dac32d7f
sebaxx85 <isurus.1313@gmail.com>
2025-02-15 15:12:57 -0300

    error hide

--- src/app/table-errors/table-errors.component.ts
+++ src/app/table-errors/table-errors.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { SharedModule } from '../Shared/shared.module';
 import { MatIconModule } from '@angular/material/icon';
@@ -29,12 +29,10 @@ import { IntercambioDatosService } from '../services/intercambio-datos.service';
   templateUrl: './table-errors.component.html',
   styleUrl: './table-errors.component.scss'
 })
-export class TableErrorsComponent {
+export class TableErrorsComponent implements OnInit{
 
   title = "Errores Cometidos";
   loading = false;
-  today = new Date();
-  cantidadPreguntas = 0;
   cantidadArray: number[] = [];
   nivelIdioma = '';
   nivelIdiomaPuntaje = 1.0;
@@ -53,16 +51,21 @@ export class TableErrorsComponent {
   private dataLevel1Service = inject(DataLevel1Service);
   private intercambioDatosService = inject(IntercambioDatosService);
 
+  displayedColumns: string[] = ['numero', 'error', 'correccion'];
+  errores: { numero: number, error: string, correccion: string }[] = [];
+
   ngOnInit() {
     this.respuestasIncorrectas = this.intercambioDatosService.getCantidadRespIncorrectas();
-   }
+    console.log(this.respuestasIncorrectas);
+    
+    // Generar filas dinámicamente
+    this.errores = Array.from({ length: this.respuestasIncorrectas }, (_, i) => ({
+      numero: i + 1, // Número de la fila (1, 2, 3, ...)
+      error: '', // Inicialmente vacío
+      correccion: '' // Inicialmente vacío
+    }));
+  }
 
-  displayedColumns: string[] = ['numero', 'error', 'correccion'];
-  errores = [
-    { numero: 1, error: 'Palabra mal escrita', correccion: 'Palabra corregida' },
-    { numero: 2, error: 'Respuesta incorrecta', correccion: 'Respuesta correcta' },
-    { numero: 3, error: 'Error de sintaxis', correccion: 'Corrección aplicada' }
-  ];
 
   volver() {
     this.loading = true;
