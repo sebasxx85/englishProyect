defb5a2bd7dabfa995a63ca6397529ae326836ae
sebastian <isurus.1313@gmail.com>
2025-02-26 13:34:30 -0300

    add ita y por

---
+++ src/app/components/ItalianType/type1-ita/type1-ita.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-type1-ita',
+  standalone: true,
+  imports: [],
+  templateUrl: './type1-ita.component.html',
+  styleUrl: './type1-ita.component.scss'
+})
+export class Type1ItaComponent {
+
+}
---
+++ src/app/components/PortuguesType/type1-por/type1-por.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-type1-por',
+  standalone: true,
+  imports: [],
+  templateUrl: './type1-por.component.html',
+  styleUrl: './type1-por.component.scss'
+})
+export class Type1PorComponent {
+
+}
