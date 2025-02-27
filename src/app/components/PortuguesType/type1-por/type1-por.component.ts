c8a3a86514222010a3f6afecfa02f8636d9c7d0d
sebastian <isurus.1313@gmail.com>
2025-02-27 13:18:01 -0300

    add portugues

--- src/app/components/PortuguesType/type1-por/type1-por.component.ts
+++ src/app/components/PortuguesType/type1-por/type1-por.component.ts
@@ -1,12 +1,230 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ShufflePipe2 } from 'src/app/pipes/shuffle2.pipe';
+import { DataPortug1Service } from 'src/app/services/data-portug1.service';
+import { IntercambioDatosService } from 'src/app/services/intercambio-datos.service';
+import { SharedModule } from 'src/app/Shared/shared.module';
 
 @Component({
   selector: 'app-type1-por',
   standalone: true,
-  imports: [],
+   imports: [CommonModule, ShufflePipe2, MatCardModule, ReactiveFormsModule, SharedModule],
   templateUrl: './type1-por.component.html',
   styleUrl: './type1-por.component.scss'
 })
-export class Type1PorComponent {
+export class Type1PorComponent implements OnInit {
+  
+    title = "Realizando Prueba";
+    loading = false;
+    form!: FormGroup;
+    today = new Date();
+    cantidadPreguntas = 0;
+    cantidadArray: number[] = [];
+    nivelIdioma = '';
+  
+    // Arreglos de preguntas y respuestas
+    dataTablePreguntas: string[] = [];
+    dataTableRespuestas: string[] = [];
+    currentPage = 0;
+    preguntasPorPagina = 3;
+  
+    //Obtener respuestas correctas e incorrectas
+    respuestasCorrectas = 0;
+    respuestasIncorrectas = 0;
+    private subscription: Subscription = new Subscription();
+  
+    contador = 0;
+  
+    // Inyecciones
+    private fb = inject(FormBuilder);
+    private router = inject(Router);
+    private dataLevel1Service = inject(DataPortug1Service);
+    private intercambioDatosService = inject(IntercambioDatosService);
+  
+    ngOnInit() {
+      this.initForm();
+      this.cantidadPreguntas = this.intercambioDatosService.getCantidad();
+      this.nivelIdioma = this.intercambioDatosService.getNivelIdioma();
+      this.cargarNivelPortugues();
+  
+      // Crear un array basado en la cantidad de preguntas
+      this.cantidadArray = Array.from({ length: this.cantidadPreguntas }, (_, index) => index);
+  
+      // Crear dinámicamente los controles de formulario para cada pregunta
+      this.cantidadArray.forEach(index => {
+        this.form.addControl(`respuesta${index}`, this.fb.control(''));
+      });
+  
+      //subscribirse al valor de respuestas correctas e incorrectas
+      this.subscription.add(
+        this.intercambioDatosService.respuestasCorrectas$.subscribe(
+          (valor) => this.respuestasCorrectas = valor
+        )
+      );
+  
+      this.subscription.add(
+        this.intercambioDatosService.respuestasIncorrectas$.subscribe(
+          (valor) => this.respuestasIncorrectas = valor
+        )
+      );
+  
+      this.terminar();
+  
+    }
+  
+    initForm() {
+      this.form = this.fb.group({
+        fechaInicio: [this.today.toISOString().split('T')[0]],
+        pregunta: ['Italiano'],
+        respuesta: ['Principiante'],
+        cantidad: ['1'],
+        time: ['true']
+      });
+    }
+  
+    // Función para cargar preguntas aleatorias y sus respuestas correspondientes
+    cargarNivelPortugues() {
+      let preguntas: string[] = [];
+      let respuestas: string[] = [];
+  
+      // Obtener las preguntas y respuestas según el nivel de idioma
+      if (this.nivelIdioma === 'principiante') {
+        preguntas = this.dataLevel1Service.getWordsPrincipiantes();
+        respuestas = this.dataLevel1Service.getWordsPrincipiantesRes();
+      } else if (this.nivelIdioma === 'basico') {
+        preguntas = this.dataLevel1Service.getWordsBasico();
+        respuestas = this.dataLevel1Service.getWordsBasicoRes();
+      } else if (this.nivelIdioma === 'basico-alto') {
+        preguntas = this.dataLevel1Service.getWordsBasicoAlto();
+        respuestas = this.dataLevel1Service.getWordsBasicoAltoRes();
+      } else if (this.nivelIdioma === 'intermedio') {
+        preguntas = this.dataLevel1Service.getWordsintermedio();
+        respuestas = this.dataLevel1Service.getWordsintermedioRes();
+      // } else if (this.nivelIdioma === 'intermedio-alto') {
+      //   preguntas = this.dataLevel1Service.getWordsintermedioAlto();
+      //   respuestas = this.dataLevel1Service.getWordsIntermedioAltoRes();
+      } else {
+        console.log("Nivel no reconocido");
+        this.loading = true;
+        
+        setTimeout(() => {
+          this.router.navigate(['/home']);
+        }, 1000);
+        return;
+      }
+  
+      // Obtener preguntas aleatorias
+      this.dataTablePreguntas = this.getRandomWords(preguntas, this.cantidadPreguntas);
+  
+      // Buscar las respuestas correspondientes a las preguntas aleatorias
+      this.dataTableRespuestas = this.buscarRespuestas(this.dataTablePreguntas, preguntas, respuestas);
+    }
+  
+    // Método para obtener palabras aleatorias
+    getRandomWords(words: string[], count: number): string[] {
+      const shuffled = [...words].sort(() => 0.5 - Math.random()); // Barajar las palabras
+      return shuffled.slice(0, count); // Retornar las primeras `count` palabras
+    }
+  
+    // Método para buscar las respuestas correspondientes a las preguntas aleatorias
+    buscarRespuestas(preguntasSeleccionadas: string[], todasLasPreguntas: string[], todasLasRespuestas: string[]): string[] {
+      return preguntasSeleccionadas.map(pregunta => {
+        const index = todasLasPreguntas.indexOf(pregunta);
+        return todasLasRespuestas[index] || '';
+      });
+    }
+  
+  
+  
+    //Logica de contador que cuando llega a cero se va a result
+    terminar(){
+      this.contador = this.intercambioDatosService.getResultado();
+  
+      // Si el tiempo es mayor a cero, inicia el contador
+      if (this.contador > 0) {
+        const interval = setInterval(() => {
+          this.contador -= 1;
+          if (this.contador <= 0) {
+            clearInterval(interval); // Detener el intervalo cuando llega a cero
+            this.enviar();
+          }
+        }, 1000); // Reducir el contador cada segundo
+      }
+    }
+  
+    //////// Logica carrusel //////
+  
+    getCurrentPageQuestions() {
+      const start = this.currentPage * this.preguntasPorPagina;
+      const end = start + this.preguntasPorPagina;
+      return Array.from({ length: this.dataTablePreguntas.slice(start, end).length }, (_, i) => start + i);
+    }
+  
+  
+    nextPage() {
+      const totalPages = Math.ceil(this.dataTablePreguntas.length / this.preguntasPorPagina);
+      if (this.currentPage < totalPages - 1) {
+        this.currentPage++;
+      }
+    }
+  
+    previousPage() {
+      if (this.currentPage > 0) {
+        this.currentPage--;
+      }
+    }
+  
+    isLastPage(): boolean {
+      const totalPages = Math.ceil(this.dataTablePreguntas.length / this.preguntasPorPagina);
+      return this.currentPage === totalPages - 1;
+    }
+  
+    //////////////// Fin Logica carrusel ///////////////
+  
+    enviar() {
+  
+      this.loading = true;
+      this.contador = 0;
+      this.respuestasCorrectas = 0;
+      this.respuestasIncorrectas = 0;
+    
+      let respuestasIncorrectasArray: { pregunta: string, respuestaUsuario: string, respuestaCorrecta: string }[] = [];
+    
+      // Contar respuestas correctas e incorrectas y guardar las incorrectas
+      this.cantidadArray.forEach((index) => {
+        const respuestaSeleccionada = this.form.get(`respuesta${index}`)?.value;
+        const respuestaCorrecta = this.dataTableRespuestas[index];
+    
+        if (respuestaSeleccionada === respuestaCorrecta) {
+          this.respuestasCorrectas++;
+        } else {
+          this.respuestasIncorrectas++;
+          respuestasIncorrectasArray.push({
+            pregunta: this.dataTablePreguntas[index],
+            respuestaUsuario: respuestaSeleccionada && respuestaSeleccionada.trim() !== '' ? respuestaSeleccionada : 'No contestada',
+            respuestaCorrecta: respuestaCorrecta
+          });
+        }
+      });
+    
+      // Guardar los valores en el servicio
+      this.intercambioDatosService.setCantidadRespCorrectas(this.respuestasCorrectas);
+      this.intercambioDatosService.setCantidadRespIncorrectas(this.respuestasIncorrectas);
+      this.intercambioDatosService.setRespuestasIncorrectasArray(respuestasIncorrectasArray);
+    
+      setTimeout(() => {
+        this.router.navigate(['/result']);
+      }, 1000);
+    }
+    
+  
+    ngOnDestroy() {
+      // Cancelar suscripción para evitar fugas de memoria
+      this.subscription.unsubscribe();
+    }
 
 }
--- src/app/services/data-portug1.service.ts
+++ src/app/services/data-portug1.service.ts
@@ -5,5 +5,137 @@ import { Injectable } from '@angular/core';
 })
 export class DataPortug1Service {
 
-  constructor() { }
+  private principianteA0: string[] = [
+    'maçã', 'banana', 'cereja', 'tâmara', 'elefante', 'peixe', 'uva', 'casa', 'ilha', 'jaqueta',
+    'canguru', 'limão', 'macaco', 'caderno', 'laranja', 'lápis', 'rainha', 'coelho', 'sol', 'tigre',
+    'azul', 'vermelho', 'verde', 'rei'
+  ];
+  
+  private basicoA1: string[] = [
+    'gato', 'cachorro', 'livro', 'carro', 'árvore', 'água', 'mesa', 'janela', 'porta', 'cadeira',
+    'escola', 'bola', 'flor', 'rua', 'relógio', 'xícara', 'camisa', 'sapato', 'cama', 'lâmpada',
+    'vinho', 'dormir', 'sonho', 'também', 'goma de mascar'
+  ];
+  
+  private basicoAltoA2: string[] = [
+    'guardanapo', 'de jeito nenhum', 'lamentou', 'poluição', 'por um bom tempo', 'cabana', 'tenho que', 'ameaça', 'dever', 'hoje em dia',
+    'tendência', 'filmagem', 'testa', 'comprador compulsivo', 'dobrar', 'enganar', 'felizmente', 'decepcionante', 'tricotar', 'casa',
+    'rainha', 'coelho', 'bandeja', 'sofá', 'lagosta', 'uma vez', 'ambos', 'almôndegas', 'também', 'longe',
+    'cidade natal', 'desaparecer', 'chaveiro', 'micro-ondas', 'costurar', 'nojento', 'seda', 'de qualquer forma', 'amargo', 'farinha',
+    'sugerir', 'repentino', 'ritmo', 'desencorajado', 'no entanto', 'à prova d\'água', 'horrível', 'apetite', 'apesar de', 'obra-prima',
+    'garantir', 'loucura', 'apoiar', 'desejo', 'assegurar', 'cineasta', 'dolorido', 'duquesa', 'certo', 'declaração', 'digno', 'assustador',
+    'aquário', 'coroa', 'surdo', 'calor', 'frutos do mar', 'ingênuo', 'segurança', 'local', 'pista', 'suspeito', 'ganancioso', 'fracasso'
+  ];
+  
+  private intermedioB1: string[] = [
+    'gravata', 'adiar', 'principalmente', 'desafiador', 'chateado', 'com antecedência', 'confiável', 'egoísta', 'para-choque', 'provável', 
+    'enquanto isso', 'parcialmente', 'contanto que', 'encorajador', 'emboscada', 'laticínios', 'contente', 'caranguejo', 'processar', 'ilha',
+    'jaqueta', 'rainha', 'coelho', 'lápis', 'limão', 'papelão', 'supostamente', 'afiado', 'entre', 'amigo',
+    'sombrio', 'qualificar', 'lustre', 'pecado', 'buquê', 'noiva', 'toalha de mesa', 'desastre', 'rachaduras', 'porão',
+    'descobrir', 'também', 'frequentemente', 'sobras', 'mastigar', 'libertação', 'estragar', 'discurso', 'pepino', 'fofoca',
+    'gado', 'campo', 'tornozelo', 'talheres', 'granulado', 'hesitar', 'atencioso', 'alívio', 'cerâmica',
+    'principal', 'fora de estrada', 'embora', 'barraca', 'corretamente', 'exceder', 'útil', 'adivinhar',
+    'sentido', 'preso', 'desesperadamente', 'raio', 'boa tarde', 'bom dia', 'boa noite', 'discutir',
+    'cruzamento', 'pular', 'salto', 'qualquer coisa', 'fique quieto', 'cale-se', 'vale a pena', 'fraude', 'trilha', 'venda', 'maquiagem',
+    'farmácia', 'derramamento', 'molho', 'inofensivo', 'artigos de higiene', 'escovado', 'barba', 'fio dental', 'patinar', 'esqui',
+    'júri', 'educado', 'decolar', 'alho', 'menos', 'assar', 'libras', 'fatia', 'litros', 'ladrão', 'culpa',
+    'alcançar', 'aconchegante', 'confortável', 'poste', 'aquecedor', 'frio', 'lareira', 'arruinar', 'arco', 'morto', 'fiação',
+    'elegante', 'campainha', 'linha de montagem', 'alcançar', 'riscar', 'banheira', 'violação', 'vitrine', 'fogão', 'dar um passo à frente',
+    'recibo', 'roubo', 'cenouras', 'separação', 'receita', 'apressar-se', 'conhecimento', 'ligação', 'retrato',
+    'cético', 'além disso', 'defeitos', 'fila', 'empreendimento', 'padrões', 'refém', 'estaca', 'segurar', 'no geral', 'padaria'
+  ];
+  
+
+ //Metodos Preguntas
+
+ getWordsPrincipiantes(): string[] {
+  return this.principianteA0;
+}
+
+getWordsBasico(): string[] {
+  return this.basicoA1;
+}
+
+getWordsBasicoAlto(): string[] {
+  return this.basicoAltoA2;
+}
+
+getWordsintermedio(): string[] {
+  return this.intermedioB1;
+}
+
+/* getWordsintermedioAlto(): string[] {
+  return this.intermedioAltoB2;
+}
+ */
+
+
+//Repuestas
+private principianteA0Res: string[] = [
+  'manzana', 'banana', 'cereza', 'fecha', 'elefante', 'pez', 'uva', 'casa', 'isla', 'chaqueta', 
+  'canguro', 'limón', 'mono', 'cuaderno','naranja', 'lápiz', 'reina', 'conejo', 'sol', 'tigre',
+  'azul', 'rojo', 'verde','rey'
+];
+
+private basicoA1Res: string[] = [
+  'gato', 'perro', 'libro', 'coche', 'árbol', 'agua', 'mesa','ventana', 'puerta', 'silla', 
+  'escuela', 'pelota', 'flor', 'calle','reloj', 'taza', 'camisa', 'zapato', 'cama', 'lámpara',
+  'vino','dormir', 'sueño', 'tambien', 'chicle/ goma'
+];
+
+private basicoAltoA2Res: string[] = [
+  'servilleta', 'de ninguna manera', 'lamentó', 'contaminación', 'bastante tiempo', 'choza', 'tengo que', 'amenaza','tener que', 'hoy en día', 
+  'tendencia', 'metraje', 'frente', 'comprador compulsivo', 'doblar', 'engañar','afortunadamente', 'decepcionante', 'tejer', 'casa', 
+  'reina', 'conejo', 'bandeja', 'sofá', 'langosta', 'una vez', 'ambos', 'albóndigas', 'también', 'lejos', 
+  'ciudad natal', 'desaparecer', 'llavero', 'microondas', 'coser', 'asqueroso/ bruto', 'seda', 'de todos modos', 'amargo','harina',
+  'sugerir','repentino', 'ritmo', 'desanimado', 'sin embargo', 'impermeable', 'espantoso', 'apetito', 'a pesar de', 'obra maestra',
+  'asegurar', 'locura', 'apoyo', 'deseo', 'asegurar','cineasta', 'adolorido', 'duquesa', 'cierto/certero', 'declaración', 'valioso/digno',
+  'escalofriante', 'pecera', 'corona', 'sordo', 'calidez', 'mariscos', 'crédulo', 'guardaespaldas', 'lugar/sitio', 'pista', 'sospechoso',
+  'codicioso', 'fallo/fracaso'
+
+];
+
+private intermedioB1Res: string[] = [
+  'corbata', 'posponer', 'mayormente', 'desafiante', 'molesto', 'con anticipación', 'confiable', 'egoísta', 'parachoque','probable', 
+  'mientras tanto', 'parcialmente', 'siempre que', 'alentador', 'emboscada', 'lácteos', 'contento', 'cangrejo', 'demandar', 'isla', 
+  'chaqueta', 'reina', 'conejo', 'lápiz', 'limón', 'cartón', 'supuestamente', 'afilado', 'entre', 'amigo', 
+  'sombra', 'calificar', 'candelabro', 'pecado', 'ramo', 'novia', 'mantel', 'desastre', 'grietas', 'sótano',
+  'descubrir/ averiguar', 'también', 'a menudo', 'sobras', 'masticar', 'liberación', 'arruinar/ estropear','dar discurso', 'pepino', 'chisme',
+  'ganado', 'campo', 'tobillo', 'cubiertos', 'chispitas', 'dudar', 'considerado', 'alivio', 'cerámica',
+
+  'principal', 'fuera de camino', 'aunque', 'parar', 'propiamente', 'superar', 'útil', 'adivinar', 'sentido', 'atrapado', 'sin esperanza',
+  'relámpago', 'buenas tardes', 'buenos días', 'buenas noches', 'conversar/discutir', 'cruce/encrucijada', 'saltar', 'saltar', 'cualquiera',
+  'cállate', 'cállate', 'vale la pena', 'engaño/impostor', 'camino/pista', 'oferta/descuento', 'maquillaje', 'farmacia',
+  'derramar', 'ponerse elegante', 'inofensivo', 'artículos de aseo', 'cepillado', 'barba', 'hilo dental', 'patinar', 'esquiando', 'jurado',
+  'cortés', 'quitarse/despegar', 'ajo', 'menos', 'asando', 'libras', 'rebanada', 'litros', 'ladrón', 'culpa', 'alcanzar', 'acogedor', 'cómodo',
+  'poste/polo', 'calefactor', 'frío', 'chimenea', 'ruina', 'arco', 'falleció', 'alambrado', 'elegante', 'timbre', 'línea de ensamblaje', 'lograr',
+  'rascar', 'bañera', 'brecha', 'vitrina', 'estufa', 'un paso adelante', 'recibo', 'robando', 'zanahorias', 'ruptura/separación',
+  'receta', 'apurarse', 'conocimiento', 'llamada/decisión', 'retrato', 'escéptico', 'además', 'defectos','cola', 'empresa/aventura', 'patrones',
+  'rehén', 'apuesta/riesgo', 'sostener', 'en general', 'panadería'
+];
+
+//Metodos Respuestas
+
+getWordsPrincipiantesRes(): string[] {
+  return this.principianteA0Res;
+}
+
+getWordsBasicoRes(): string[] {
+  return this.basicoA1Res;
+}
+
+getWordsBasicoAltoRes(): string[] {
+  return this.basicoAltoA2Res;
+}
+
+getWordsintermedioRes(): string[] {
+  return this.intermedioB1Res;
+}
+
+/* getWordsIntermedioAltoRes(): string[] {
+  return this.intermedioAltoB2Res;
+}
+ */
+
+  
 }
