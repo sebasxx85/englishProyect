0b56e727ed21b69e228596b0e73594d6213794d5
sebastian <isurus.1313@gmail.com>
2025-02-28 16:26:17 -0300

    add curs y rank

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SharedModule } from 'src/app/Shared/shared.module';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss'
 })
 export class CursosComponent {
 
+  title = "Cursos Premium";
 }
--- src/app/components/ranking/ranking.component.ts
+++ src/app/components/ranking/ranking.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SharedModule } from 'src/app/Shared/shared.module';
 
 @Component({
   selector: 'app-ranking',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule],
   templateUrl: './ranking.component.html',
   styleUrl: './ranking.component.scss'
 })
 export class RankingComponent {
+  title = "Ranking";
 
 }
