f83aad24a50c73c535c6cbb955a2143079a08503
sebaxx85 <isurus.1313@gmail.com>
2025-03-04 19:28:09 -0300

    add cursos

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -1,15 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { CursosService } from 'src/app/services/cursos.service';
 import { SharedModule } from 'src/app/Shared/shared.module';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, MatCardModule, FormsModule],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.scss'
 })
 export class CursosComponent {
-
+  
   title = "Cursos Premium";
+  cursos: any[] = [];
+  cursosFiltrados: any[] = [];
+  terminoBusqueda: string = '';
+
+  private cursosService = inject(CursosService);
+
+
+  ngOnInit(): void {
+    this.cursos = this.cursosService.obtenerCursos();
+    this.cursosFiltrados = [...this.cursos];
+  }
+
+  filtrarCursos(termino: string) {
+    if (termino.trim() === '') {
+      this.cursosFiltrados = [...this.cursos];
+    } else {
+      this.cursosFiltrados = this.cursos.filter(curso =>
+        curso.categoria.toLowerCase().includes(termino.toLowerCase())
+      );
+    }
+  }
+
+  eliminarCurso(id: number) {
+    this.cursosService.eliminarCurso(id);
+    this.cursos = this.cursosService.obtenerCursos();
+    this.cursosFiltrados = [...this.cursos];
+  }
+
+
 }
---
+++ src/app/services/cursos.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  MisCursos = [
+    { id: 1, categoria: 'Ingles', curso: 'Producto 1', sitio: 'http://example.com/curso1', imagen: 'assets/images/Curso.png', estrellas: 'assets/images/2Estrellas.png' },
+    { id: 2, categoria: 'Ingles', curso: 'Producto 2', sitio: 'http://example.com/curso2', imagen: 'assets/images/Curso.png', estrellas: 'assets/images/3Estrellas.png' },
+    { id: 3, categoria: 'Portugues', curso: 'Producto 3', sitio: 'http://example.com/curso3', imagen: 'assets/images/Curso.png', estrellas: 'assets/images/5Estrellas.png' },
+    { id: 4, categoria: 'Italiano', curso: 'Producto 4', sitio: 'http://example.com/curso4', imagen: 'assets/images/Curso.png', estrellas: 'assets/images/4Estrellas.png' },
+  ];
+  
+  
+
+
+  obtenerCursos() {
+    // con ... Retorna una copia del array para evitar modificaciones externas
+    return [...this.MisCursos];
+  }
+
+  agregarCurso(curso: any) {
+    //Comprobar si existe
+    const existe = this.MisCursos.some(c => c.id === curso.id);
+
+    if (!existe) {
+      this.MisCursos.push(curso);
+    } else {
+      console.log(`El curso con id ${curso.id} ya existe.`);
+    }
+  }
+
+  eliminarCurso(id: number) {
+    //Comprobar si existe
+    const index = this.MisCursos.findIndex(curso =>
+      curso.id === id
+    );
+
+    if (index !== -1) {
+      this.MisCursos.splice(index, 1);
+    }
+  }
+
+  encontrarCursosByCategory(categoria: string): any[] {
+    return this.MisCursos.filter(curso =>
+      curso.categoria === categoria);
+  }
+
+
+  editarCurso(id: number, nuevaData: any) {
+    const index = this.MisCursos.findIndex(curso => curso.id === id);
+
+    if (index !== -1) {
+      this.MisCursos[index] = { ...this.MisCursos[index], ...nuevaData };
+    } else {
+      console.log(`No se encontró el curso con id ${id}`);
+    }
+  }
+
+
+
+}
