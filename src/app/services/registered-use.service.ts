51f33442ae5636bcb6cdf2367df4ebe5a6f5eb9a
sebaxx85 <isurus.1313@gmail.com>
2025-03-05 00:07:35 -0300

    add signal

--- src/app/services/registered-use.service.ts
+++ src/app/services/registered-use.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisteredUseService {
 
-  constructor() { }
+  usuario = signal<string | null>(null); // Solo almacena el nombre de usuario
+
+  login(usuario: string, password: string) {
+    // Simulación de autenticación (en un caso real, iría una petición HTTP)
+    if (usuario === 'admin' && password === '1234') { 
+      this.usuario.set(usuario); // Guarda solo el usuario x seguridad
+    } else {
+      console.error('Credenciales incorrectas');
+    }
+  }
+
+  logout() {
+    this.usuario.set(null);
+  }
+
 }
