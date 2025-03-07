e5e7551ec8436c38030905557cefcdea66b07791
sebastian <isurus.1313@gmail.com>
2025-03-07 08:14:22 -0300

    dashborad update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,15 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { RegisteredUseService } from 'src/app/services/registered-use.service';
+import { SharedModule } from 'src/app/Shared/shared.module';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  title = "Panel de Control";
 
   public registerUser = inject(RegisteredUseService)
   private router = inject(Router)
