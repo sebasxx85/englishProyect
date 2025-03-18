c34c1c23e13f28de0023bdc688cb13c6f226aad0
sebastian <isurus.1313@gmail.com>
2025-03-18 09:15:15 -0300

    envir fixed

--- src/app/environments/environment.dev.ts
+++ src/app/environments/environment.dev.ts
@@ -3,7 +3,7 @@
 export const environment = {
     production: false,
     urlHost: 'https://nodejs-english-734228083178.us-central1.run.app/api',
-    apiUrl: 'http://localhost:8080/api/v1/users',
+    apiUrl: 'https://nodejs-english-734228083178.us-central1.run.app/api/v1/users',
     userId: 1
     // otras variables de configuración
   };
