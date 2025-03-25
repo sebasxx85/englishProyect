7085a6ebcc0587753d5705e13d2afdba82ed46d8
sebaxx85 <isurus.1313@gmail.com>
2025-03-25 18:53:27 -0300

    dashboard fixed

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { RegisteredUseService } from 'src/app/services/registered-use.service';
 import { SharedModule } from 'src/app/Shared/shared.module';
@@ -11,18 +11,26 @@ import { SharedModule } from 'src/app/Shared/shared.module';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
   title = "Panel de Control";
 
-  public registerUser = inject(RegisteredUseService)
+  public nombreUsuario: string | null = null;
+
+  private registerUser = inject(RegisteredUseService)
   private router = inject(Router)
+
+  ngOnInit(): void {
+    // Cargar desde localStorage al iniciar
+    this.nombreUsuario = localStorage.getItem('usuarioLogueado');
+  }
 
   logout() {
     setTimeout(() => {
-    this.registerUser.logout();
-    this.router.navigate(['/home']);
-  }, 2000);
-}
+      this.registerUser.logout();
+      this.nombreUsuario = null;
+      this.router.navigate(['/home']);
+    }, 2000);
+  }
 
 }
