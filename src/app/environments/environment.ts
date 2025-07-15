cc47ee214086e67d1aa06efe4092296b8430aea2
sebastian <isurus.1313@gmail.com>
2025-07-15 18:06:30 -0400

    add environment

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,7 +1,8 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://nodejs-english-734228083178.us-central1.run.app/api/',
+    apiUrl: 'http://localhost:3000/api/',
+    //apiUrl: 'https://nodejs-english-734228083178.us-central1.run.app/api/',
     userId: 1
   };
   
