5b7a1b072038002aaa8ca8395082ce1c7cb405e1
sebastian <isurus.1313@gmail.com>
2025-10-13 00:29:34 -0300

    angular universal updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -49,6 +49,7 @@ import { ShufflePipe } from './pipes/shuffle.pipe';
         MatNativeDateModule,
         ReactiveFormsModule,
         MatProgressSpinnerModule], providers: [
-        provideHttpClient(withInterceptorsFromDi())
+        provideHttpClient(withInterceptorsFromDi()),
+        provideClientHydration()
     ] })
 export class AppModule { }
